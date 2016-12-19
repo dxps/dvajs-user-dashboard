4ad2269e79f2582a3c4cf1fab342dc16651248b1
visvadw <visvadw@gmail.com>
2016-12-19 22:18:16 +0200

    font-size-base increased to 13px, line-height-base decreased to 1.2

--- antd-theme.js
+++ antd-theme.js
@@ -4,6 +4,8 @@ module.exports = () => {
         '@link-color': '#1DA57A',
         '@border-radius-base': '2px',
         '@icon-url': '"/assets/fonts/iconfont/iconfont"',
-        '@font-family': '"LatoWeb"'
+        '@font-family': '"LatoWeb"',
+        '@font-size-base': '13px',
+        '@line-height-base': '1.2'
     };
 };
