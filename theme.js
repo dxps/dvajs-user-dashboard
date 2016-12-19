5a863f2e465305816b6e1cf1a382556923d5bcbc
visvadw <visvadw@gmail.com>
2016-12-19 16:43:48 +0200

    Lato fonts used by default, favicon included

--- theme.js
+++ theme.js
@@ -1,8 +1,9 @@
 module.exports = () => {
-  return {
-    '@primary-color': '#1DA57A',
-    '@link-color': '#1DA57A',
-    '@border-radius-base': '2px',
-    '@icon-url': '"/assets/fonts/iconfont/iconfont"',
-  };
+    return {
+        '@primary-color': '#1DA57A',
+        '@link-color': '#1DA57A',
+        '@border-radius-base': '2px',
+        '@icon-url': '"/assets/fonts/iconfont/iconfont"',
+        '@font-family': '"LatoWeb"'
+    };
 };
